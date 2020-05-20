3a89537e0b0e1373fb2b45c26d6dd0099cf5b11f
David Yu <david.yu@yale.edu>
2020-05-20 18:33:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,14 @@ require "yaml"
 def load_library(path)
   # code goes here
   file = YAML.load_file(path)
-  file.each = 
+  output = {}
+  file.each do |name, emoticon_list|
+    output[name] = {
+      english: emoticon_list[0]
+      japanese: emoticon_list[1]
+    }
+  end
+  output
 end
 
 def get_japanese_emoticon
